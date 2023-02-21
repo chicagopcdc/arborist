cfe2ea49703e48a74eaaff8873514ddd039226d5
grugna <graglia01@gmail.com>
2023-02-21 10:44:07 -0600

    Update auth.go

--- arborist/auth.go
+++ arborist/auth.go
@@ -695,7 +695,7 @@ func authMapping(db *sqlx.DB, username string, server *Server) (AuthMapping, *Er
 		INNER JOIN policy_role ON policy_role.policy_id = policies.policy_id
 		INNER JOIN permission ON permission.role_id = policy_role.role_id
 		INNER JOIN resource ON resource.path <@ roots.path
-		where ltree2text(resource.path) not like all (ARRAY['programs.pcdc.projects.20220201.%', 'programs.pcdc.projects.20220808.%', 'programs.pcdc.projects.20220110.%', 'programs.pcdc.projects.20220501_1.%', 'programs.pcdc.projects.20211006.%', 'programs.pcdc.projects.20210915.%', 'programs.pcdc.projects.20210212.%']);
+		where ltree2text(resource.path) not like all (ARRAY['programs.pcdc.projects.20220201.%', 'programs.pcdc.projects.20220808.%', 'programs.pcdc.projects.20220110.%', 'programs.pcdc.projects.20220501_S01.%', 'programs.pcdc.projects.20211006.%', 'programs.pcdc.projects.20210915.%', 'programs.pcdc.projects.20210212.%']);
 	`
 	
 	// where resource.path ~ (CAST('programs.pcdc.projects.20230228.*' AS lquery))
